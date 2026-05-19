35200caa29e2628e122fcbee6f34194d8b0797f5
Luka Kanjir <luka.kanjir5@gmail.com>
2026-05-19 16:35:03 +0200

    Fix unauthorized GET in basket.ts

--- routes/basket.ts
+++ routes/basket.ts
@@ -6,22 +6,29 @@
 import { type Request, type Response, type NextFunction } from 'express'
 import { ProductModel } from '../models/product'
 import { BasketModel } from '../models/basket'
-import * as challengeUtils from '../lib/challengeUtils'
 
 import * as utils from '../lib/utils'
 import * as security from '../lib/insecurity'
-import { challenges } from '../data/datacache'
 
 export function retrieveBasket () {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       const id = req.params.id
-      const basket = await BasketModel.findOne({ where: { id }, include: [{ model: ProductModel, paranoid: false, as: 'Products' }] })
-      /* jshint eqeqeq:false */
-      challengeUtils.solveIf(challenges.basketAccessChallenge, () => {
-        const user = security.authenticatedUsers.from(req)
-        return user && id && id !== 'undefined' && id !== 'null' && id !== 'NaN' && user.bid && user?.bid != parseInt(id, 10) // eslint-disable-line eqeqeq
+      const user = security.authenticatedUsers.from(req)
+      if (!user?.data?.id) {
+        res.status(401).json({ status: 'error', message: 'Authentication required.' })
+        return
+      }
+
+      const basket = await BasketModel.findOne({
+        where: { id, UserId: user.data.id },
+        include: [{ model: ProductModel, paranoid: false, as: 'Products' }]
       })
+      if (basket == null) {
+        res.status(404).json({ status: 'error', message: 'Basket not found.' })
+        return
+      }
+
       if (((basket?.Products) != null) && basket.Products.length > 0) {
         for (let i = 0; i < basket.Products.length; i++) {
           basket.Products[i].name = req.__(basket.Products[i].name)
